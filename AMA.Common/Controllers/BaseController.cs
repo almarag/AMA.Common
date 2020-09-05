67c6b40bc934140b155b8ebca5772728fccb3177
Alejandro Martinez <almarag@gmail.com>
2020-09-05 12:12:13 -0500

    Added BaseController

---
+++ AMA.Common/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿namespace AMA.Common.Controllers
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class BaseController : Controller
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+    }
+}
