5b9f321c105632304d05f74788c149562ad14c29
Alejandro Martinez <almarag@gmail.com>
2020-09-06 23:04:42 -0500

    Implemented FluentValidation Filters

---
+++ AMA.Common/Filters/ValidateRequestFilter.cs
@@ -0,0 +1,39 @@
+﻿namespace AMA.Common.Filters
+{
+    using FluentValidation;
+    using FluentValidation.Results;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+
+    public class ValidateRequestFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var failures = new List<ValidationFailure>();
+                string message = string.Empty;
+
+                foreach (var error in context.ModelState)
+                {
+                    error.Value.Errors
+                        .ToList()
+                        .ForEach(e =>
+                        {
+                            failures.Add(new ValidationFailure(error.Key, e.ErrorMessage));
+                        });
+                }
+
+                context.Result = new ObjectResult(failures) { StatusCode = 400 };
+            }
+        }
+    }
+}
