26d1d34b12a0c3115d47f6d05ff08a249e500c6c
Alejandro Martinez <almarag@gmail.com>
2020-09-08 22:45:55 -0500

    Fixed references

--- AMA.Common/Filters/ValidateRequestFilter.cs
+++ AMA.Common/Filters/ValidateRequestFilter.cs
@@ -1,17 +1,10 @@
 ﻿namespace AMA.Common.Filters
 {
-    using FluentValidation;
     using FluentValidation.Results;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Filters;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Net;
-    using System.Net.Http;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using System.Web.Http;
-    using System.Web.Http.Controllers;
 
     public class ValidateRequestFilter : ActionFilterAttribute
     {
